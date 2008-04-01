b0ab275f236cf69dc0af9fee650d83475e286afd
cesar.gomes <cesar.gomes@3a2a2d67-9b3a-0410-8382-8b98c2212edd>
2008-04-01 23:24:21 +0000

    Moving to trunk.

---
+++ setup.py
@@ -0,0 +1,11 @@
+# Installation script
+from distutils.core import setup, Extension
+setup(
+      name='neat-python',
+      version='0.1',
+      description='A NEAT (NeuroEvolution of Augmenting Topologies) implementation',
+      packages=['neat', 'neat/iznn', 'neat/nn', 'neat/ctrnn'],
+      ext_modules=[Extension('neat/iznn/iznn_cpp', ['neat/iznn/iznn.cpp']),
+          Extension('neat/nn/nn_cpp', ['neat/nn/nn.cpp', 'neat/nn/neuron.cpp',
+              'neat/nn/synapse.cpp'])],
+)
