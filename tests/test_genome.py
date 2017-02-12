0ebbe3f9b03f5fd3a212f03a659a8702f03a18a0
CodeReclaimers <github@codereclaimers.com>
2017-02-12 10:19:57 -0500

    Added new tests for DefaultGenome.

--- tests/test_genome.py
+++ tests/test_genome.py
@@ -1,112 +1,119 @@
-import os
+import unittest
+
 import neat
+from neat.six_util import iterkeys
 
 
-# def check_simple(genome_type):
-#     local_dir = os.path.dirname(__file__)
-#     config = Config(os.path.join(local_dir, 'test_configuration'))
-#     c1 = genome_type.create_unconnected(1, config)
-#     c1.connect_full()
-#     repr(c1)
-#     str(c1)
-#
-#     # add two hidden nodes
-#     c1.add_hidden_nodes(2)
-#     repr(c1)
-#     str(c1)
-#
-#     # apply some mutations
-#     c1.mutate_add_node()
-#     c1.mutate_add_connection()
-#     repr(c1)
-#     str(c1)
-#
-#
-# def test_recurrent():
-#     check_simple(genome.Genome)
-#
-#
-# def test_feed_forward():
-#     check_simple(genome.FFGenome)
-#
-#
-# def check_self_crossover(genome_type):
-#     # Check that self-crossover produces a genetically identical child (with a different ID).
-#     local_dir = os.path.dirname(__file__)
-#     config = Config(os.path.join(local_dir, 'test_configuration'))
-#     c = genome_type.create_unconnected(1, config)
-#     c.connect_full()
-#     c.fitness = 0.0
-#
-#     cnew = c.crossover(c, 2)
-#     assert cnew.ID != c.ID
-#     assert len(cnew.conn_genes) == len(c.conn_genes)
-#     for kold, vold in cnew.conn_genes.items():
-#         print(kold, vold)
-#         print(c.conn_genes)
-#         assert kold in c.conn_genes
-#         vnew = c.conn_genes[kold]
-#         assert vold.is_same_innov(vnew)
-#
-#         assert vnew.weight == vold.weight
-#         assert vnew.in_node_id == vold.in_node_id
-#         assert vnew.out_node_id == vold.out_node_id
-#         assert vnew.enabled == vold.enabled
-#
-#     assert len(cnew.node_genes) == len(c.node_genes)
-#     for kold, vold in cnew.node_genes.items():
-#         assert kold in c.node_genes
-#         vnew = c.node_genes[kold]
-#
-#         assert vnew.ID == vold.ID
-#         assert vnew.type == vold.type
-#         assert vnew.bias == vold.bias
-#         assert vnew.response == vold.response
-#         assert vnew.activation_type == vold.activation_type
-#
-#
-# def test_recurrent_self_crossover():
-#     check_self_crossover(genome.Genome)
-#
-#
-# def test_feed_forward_self_crossover():
-#     check_self_crossover(genome.FFGenome)
-#
-#
-# def check_add_connection(genome_type, feed_forward):
-#     local_dir = os.path.dirname(__file__)
-#     config = Config(os.path.join(local_dir, 'test_configuration'))
-#     config.input_nodes = 3
-#     config.output_nodes = 4
-#     config.hidden_nodes = 5
-#     config.feedforward = feed_forward
-#     N = config.input_nodes + config.hidden_nodes + config.output_nodes
-#
-#     connections = {}
-#     for a in range(100):
-#         g = genome_type.create_unconnected(a, config)
-#         g.add_hidden_nodes(config.hidden_nodes)
-#         for b in range(1000):
-#             g.mutate_add_connection()
-#         for c in g.conn_genes.values():
-#             connections[c.key] = connections.get(c.key, 0) + 1
-#
-#     # TODO: The connections should be returned to the caller and checked
-#     # against the constraints/assumptions particular to the network type.
-#     for i in range(N):
-#         values = []
-#         for j in range(N):
-#             values.append(connections.get((i, j), 0))
-#         print("{0:2d}: {1}".format(i, " ".join("{0:3d}".format(x) for x in values)))
-#
-#
-# def test_recurrent_add_connection():
-#     check_add_connection(genome.Genome, 0)
-#
-#
-# def test_feed_forward_add_connection():
-#     check_add_connection(genome.FFGenome, 1)
-#
-#
-# if __name__ == '__main__':
-#     test_recurrent()
+class TestCreateNew(unittest.TestCase):
+    def setUp(self):
+        self.config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                  neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                                  'test_configuration')
+
+    def test_unconnected_no_hidden(self):
+        gid = 42
+        config = self.config.genome_config
+        config.initial_connection = 'unconnected'
+        config.num_hidden = 0
+
+        g = neat.DefaultGenome(gid)
+        self.assertEqual(gid, g.key)
+        g.configure_new(self.config.genome_config)
+
+        print(g)
+        self.assertEqual(set(iterkeys(g.nodes)), {0})
+        assert(not g.connections)
+
+    def test_unconnected_hidden(self):
+        gid = 42
+        config = self.config.genome_config
+        config.initial_connection = 'unconnected'
+        config.num_hidden = 2
+
+        g = neat.DefaultGenome(gid)
+        self.assertEqual(gid, g.key)
+        g.configure_new(self.config.genome_config)
+
+        print(g)
+        self.assertEqual(set(iterkeys(g.nodes)), {0, 1, 2})
+        assert(not g.connections)
+
+    def test_fully_connected_no_hidden(self):
+        gid = 42
+        config = self.config.genome_config
+        config.initial_connection = 'full'
+        config.num_hidden = 0
+
+        g = neat.DefaultGenome(gid)
+        self.assertEqual(gid, g.key)
+        g.configure_new(config)
+
+        print(g)
+        self.assertEqual(set(iterkeys(g.nodes)), {0})
+        self.assertEqual(len(g.connections), 2)
+
+        # Check that each input is connected to the output node
+        for i in config.input_keys:
+            assert((i, 0) in g.connections)
+
+
+    def test_fully_connected_hidden(self):
+        gid = 42
+        config = self.config.genome_config
+        config.initial_connection = 'full'
+        config.num_hidden = 2
+
+        g = neat.DefaultGenome(gid)
+        self.assertEqual(gid, g.key)
+        g.configure_new(config)
+
+        print(g)
+        self.assertEqual(set(iterkeys(g.nodes)), {0, 1, 2})
+        self.assertEqual(len(g.connections), 6)
+
+        # Check that each input is connected to each hidden node.
+        for i in config.input_keys:
+            for h in (1, 2):
+                assert((i, h) in g.connections)
+
+        # Check that each hidden node is connected to the output.
+        for h in (1, 2):
+            assert((h, 0) in g.connections)
+
+        # Check that inputs are not directly connected to outputs
+        for i in config.input_keys:
+            assert((i, 0) not in g.connections)
+
+    def test_partially_connected_no_hidden(self):
+        gid = 42
+        config = self.config.genome_config
+        config.initial_connection = 'partial'
+        config.connection_fraction = 0.5
+        config.num_hidden = 0
+
+        g = neat.DefaultGenome(gid)
+        self.assertEqual(gid, g.key)
+        g.configure_new(config)
+
+        print(g)
+        self.assertEqual(set(iterkeys(g.nodes)), {0})
+        self.assertLess(len(g.connections), 2)
+
+    def test_partially_connected_hidden(self):
+        gid = 42
+        config = self.config.genome_config
+        config.initial_connection = 'partial'
+        config.connection_fraction = 0.5
+        config.num_hidden = 2
+
+        g = neat.DefaultGenome(gid)
+        self.assertEqual(gid, g.key)
+        g.configure_new(config)
+
+        print(g)
+        self.assertEqual(set(iterkeys(g.nodes)), {0, 1, 2})
+        self.assertLess(len(g.connections), 6)
+
+
+if __name__ == '__main__':
+    unittest.main()
