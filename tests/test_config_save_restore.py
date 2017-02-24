6af6e6413b9259a4405c2961c7dcb5d8f5c3e737
Sergei Volodin <etoestja@yandex.ru>
2017-02-24 19:31:43 +0300

    Add try-except for test_config_save_restore.py

--- tests/test_config_save_restore.py
+++ tests/test_config_save_restore.py
@@ -1,14 +1,26 @@
 import neat
-config_filename_initial = 'test_configuration'
-config_filename_save = 'save_configuration'
 
-# Load initial configuration from file
-config_initial = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                             neat.DefaultSpeciesSet, neat.DefaultStagnation, config_filename_initial)
+def test_config_save_restore(config_filename_initial, config_filename_save):
+    """Check if it is possible to restore saved config"""
+    passed = False
+    try:
+        # Load initial configuration from file
+        config_initial = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                     neat.DefaultSpeciesSet, neat.DefaultStagnation, config_filename_initial)
 
-# save configuration to another file
-config_initial.save(config_filename_save)
+        # Save configuration to another file
+        config_initial.save(config_filename_save)
 
-# obtain configuration from saved file
-config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                     neat.DefaultSpeciesSet, neat.DefaultStagnation, config_filename_save)
+        # Obtain configuration from saved file
+        config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                             neat.DefaultSpeciesSet, neat.DefaultStagnation, config_filename_save)
+
+        # Test Passed
+        passed = True
+    except:
+        pass
+
+    assert passed
+
+if __name__ == '__main__':
+    test_config_save_restore('test_configuration', 'save_configuration')
