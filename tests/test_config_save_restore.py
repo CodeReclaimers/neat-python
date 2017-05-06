f437f7fb584aca90e7dc68a5b46c4fa92ca61460
CodeReclaimers <github@codereclaimers.com>
2017-05-06 12:23:01 -0400

    Added test that restored genome configuration is the identical to saved configuration.

--- tests/test_config_save_restore.py
+++ tests/test_config_save_restore.py
@@ -1,30 +1,44 @@
 import neat
 import os
+import unittest
 
-def test_config_save_restore(config_filename_initial = 'test_configuration', config_filename_save = 'save_configuration'):
-    """Check if it is possible to restore saved config"""
-    passed = False
 
-    # Get config path
-    local_dir = os.path.dirname(__file__)
-    config_path_initial = os.path.join(local_dir, config_filename_initial)
-    config_path_save = os.path.join(local_dir, config_filename_save)
+class ConfigTests(unittest.TestCase):
+    def test_config_save_restore(self):
+        """Check if it is possible to restore saved config"""
 
-    # Load initial configuration from file
-    config_initial = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                                 neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path_initial)
+        config_filename_initial = 'test_configuration'
+        config_filename_save = 'save_configuration'
 
-    # Save configuration to another file
-    config_initial.save(config_path_save)
+        # Get config path
+        local_dir = os.path.dirname(__file__)
+        config_path_initial = os.path.join(local_dir, config_filename_initial)
+        config_path_save = os.path.join(local_dir, config_filename_save)
 
-    # Obtain configuration from saved file
-    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-                         neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path_save)
+        # Load initial configuration from file
+        config_initial = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                     neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path_initial)
 
-    # Test Passed
-    passed = True
+        config1 = config_initial.genome_config
+        names1 = [p.name for p in config1._params]
+        for n in names1:
+            assert hasattr(config1, n)
 
-    assert passed
+        # Save configuration to another file
+        config_initial.save(config_path_save)
 
-if __name__ == '__main__':
-    test_config_save_restore()
+        # Obtain configuration from saved file
+        config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                             neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path_save)
+
+        config2 = config.genome_config
+        names2 = [p.name for p in config2._params]
+        for n in names2:
+            assert hasattr(config2, n)
+
+        self.assertEqual(names1, names2)
+
+        for n in names1:
+            v1 = getattr(config1, n)
+            v2 = getattr(config2, n)
+            self.assertEqual(v1, v2)
