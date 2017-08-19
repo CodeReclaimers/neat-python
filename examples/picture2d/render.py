803d89f3a2cc0a8df41d92e254ad3b88b59acc85
drallensmith <drallensmith@users.noreply.github.com>
2017-08-19 15:09:18 -0500

    Fixing master version of evolve_novelty.py

--- examples/picture2d/render.py
+++ examples/picture2d/render.py
@@ -1,4 +1,7 @@
+from __future__ import print_function
+
 import pickle
+
 import pygame
 
 import evolve
@@ -11,7 +14,7 @@ pb = evolve.PictureBreeder(128, 128, 1500, 1500, 1280, 1024, 'color', 4)
 
 with open("genome-20219-701.bin", "rb") as f:
     g = pickle.load(f)
-    print g
+    print(g)
     node_names = {0: 'x', 1: 'y', 2: 'gray'}
     visualize.draw_net(g, view=True, filename="picture-net.gv",
                        show_disabled=False, prune_unused=True, node_names=node_names)
