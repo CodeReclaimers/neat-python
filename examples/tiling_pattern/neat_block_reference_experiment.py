4e599d4bb8afe5bd66a44c53a81b0bec3c538fee
Matthijs den Toom <M.denToom@student.tudelft.nl>
2019-02-04 10:11:57 +0100

    Added 11x11 block experiment

---
+++ examples/tiling_pattern/neat_block_reference_experiment.py
@@ -0,0 +1,36 @@
+"""
+Simple example using the tile structure creation task.
+"""
+
+import os
+
+import neat
+import gym
+
+from examples.experiment_functions import NEATSwarmExperimentRunner
+from examples.experiment_template import SingleExperiment
+
+# Important variables.
+experiment_name = 'NEAT_reference_block'
+num_steps = 3000
+num_generations = 100
+num_runs = 5
+config_name = 'config-feedforward'
+
+if __name__ == '__main__':
+
+    env = gym.make('tiling-pattern11x11-block-v0')
+    runner = NEATSwarmExperimentRunner(env, num_steps)
+
+    # Create learning configuration.
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, config_name)
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+
+    # Create and run experiment.
+    experiment = SingleExperiment(config, runner, num_generations, experiment_name)
+
+    for i in range(num_runs):
+        experiment.run(experiment_name + str(i))
