3adb082a8ad77c992290ccde2e411da4b66a58d7
Matthijs den Toom <M.denToom@student.tudelft.nl>
2019-02-06 08:41:09 +0100

    Added long block experiment state_machine and free experiment

---
+++ examples/tiling_pattern/sm_block_reference_experiment_long.py
@@ -0,0 +1,38 @@
+"""
+Simple example using the tile structure creation task.
+"""
+
+import os
+
+import neat
+import gym
+
+from examples.experiment_functions import SMSwarmExperimentRunner
+from examples.experiment_template import SingleExperiment
+
+# Important variables.
+from neat.state_machine_genome import StateMachineGenome
+
+experiment_name = 'SM_4_reference_block_long'
+num_steps = 3000
+num_generations = 200
+num_runs = 1
+config_name = 'config-state_machine'
+
+if __name__ == '__main__':
+
+    env = gym.make('tiling-pattern11x11-block-v0')
+    runner = SMSwarmExperimentRunner(env, num_steps)
+
+    # Create learning configuration.
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, config_name)
+    config = neat.Config(StateMachineGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+
+    # Create and run experiment.
+    experiment = SingleExperiment(config, runner, num_generations, experiment_name)
+
+    for i in range(num_runs):
+        experiment.run(experiment_name + str(i))
