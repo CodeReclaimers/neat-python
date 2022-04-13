1d1fe5ca69a01dce2da55dea6f04122bb012779f
CodeReclaimers <CodeReclaimers@users.noreply.github.com>
2022-04-13 08:04:21 -0400

    Changed supported Python versions to 3.6 through 3.11.
    Added missing reference to original author.

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ For further information regarding general concepts and theory, please see the
 Florida (now somewhat dated), or the [publications page](https://www.kenstanley.net/papers) of his current website.
 
 `neat-python` is licensed under the [3-clause BSD license](https://opensource.org/licenses/BSD-3-Clause).  It is
-currently only supported on Python 3.6 through 3.9. 
+currently only supported on Python 3.6 through 3.11, and pypy3.
 
 ## Getting Started ##
 
--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ from setuptools import setup
 
 setup(
     name='neat-python',
-    version='0.92',
-    author='cesar.gomes, mirrorballu2',
-    author_email='nobody@nowhere.com',
-    maintainer='CodeReclaimers, LLC',
+    version='0.93',
+    author='Alan Mcintyre, Cesar Gomes Miguel, Carolina Feher da Silva, Marcio Lobo Netto',
+    author_email='alan@codereclaimers.com',
+    maintainer='Alan McIntyre',
     maintainer_email='alan@codereclaimers.com',
     url='https://github.com/CodeReclaimers/neat-python',
     license="BSD",
     description='A NEAT (NeuroEvolution of Augmenting Topologies) implementation',
     long_description='Python implementation of NEAT (NeuroEvolution of Augmenting Topologies), a method ' +
                      'developed by Kenneth O. Stanley for evolving arbitrary neural networks.',
+    long_description_content_type= 'text/x-rst',
     packages=['neat', 'neat/iznn', 'neat/nn', 'neat/ctrnn'],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -20,11 +21,12 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Scientific/Engineering'
     ]
