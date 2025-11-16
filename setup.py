227e0e19dd0f9d28d01f9c41744c4f8b696ae408
CodeReclaimers <github@codereclaimers.com>
2025-11-16 16:39:07 -0500

    Update setup.py to reflect dropping 3.6 & 3.7 support.

--- setup.py
+++ setup.py
@@ -22,8 +22,6 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
